8aecbc0423acc28e20f00103d267832768d0766d
Carlos Eduardo Coelho Freire Batista <bidu@ci.ufpb.br>
2024-06-05 20:57:36 -0300

    Create PluginEditor.h
    First version, almost empty

---
+++ plugin/include/autoremix/PluginEditor.h
@@ -0,0 +1,49 @@
+/* Description: This file contains the declaration of the 
+AutoRemixAudioProcessorEditor class, which is responsible for the GUI of the 
+plugin.
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+#include <juce_audio_basics/juce_audio_basics.h>
+#include "PluginProcessor.h"
+
+class AutoRemixAudioProcessorEditor  : public juce::AudioProcessorEditor
+{
+public:
+    AutoRemixAudioProcessorEditor (AutoRemixAudioProcessor&);
+    ~AutoRemixAudioProcessorEditor() override;
+
+    //==============================================================================
+    void paint (juce::Graphics&) override;
+    void resized() override;
+
+private:
+    AutoRemixAudioProcessor& audioProcessor;
+
+    void loadFile();
+    void onClick_Play();
+    void onClick_Stop();
+    void onClick_Save();
+    void drawAndConfigComponents();
+
+    juce::String file_path;
+
+    juce::Label file_lbl;
+    juce::Label remix_lbl;
+    
+    juce::TextButton loadfile_btn;
+    juce::TextButton play_btn;
+    juce::TextButton stop_btn;
+    juce::TextButton save_btn;
+    juce::TextButton config_btn;
+
+    juce::TextEditor file_input;
+
+    juce::TextEditor debug_text;
+
+    juce::ComboBox remix_selector;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (AutoRemixAudioProcessorEditor)
+};
